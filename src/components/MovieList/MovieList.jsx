42231ad43006f5d931a216e9b49c7282b05c3d07
Mykola T <anteycu@gmail.com>
2024-07-02 15:59:57 +0300

    implemented routing

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,17 +1,22 @@
-import { Link } from "react-router-dom";
-const MovieList = ({ moviesData = [] }) => (
-  <div>
-    <ul>
-      {moviesData.map(({ id, title, poster_path }) => (
-        <li key={id}>
-          <Link to={`movies/${id}`}>
-            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
-            <h2>{title}</h2>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+import { Link, useLocation } from "react-router-dom";
+
+const MovieList = ({ moviesData }) => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <ul>
+        {moviesData.map(({ id, title, poster_path }) => (
+          <li key={id}>
+            <Link to={location.pathname === "/" ? `movies/${id}` : `${id}`}>
+              <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
+              <h2>{title}</h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default MovieList;
