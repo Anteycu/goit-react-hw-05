e7954b9fd7c64481ce91aae934432958ed7bf04a
Mykola T <anteycu@gmail.com>
2024-07-30 16:03:53 +0300

    change moviesPage fetch way to show movies after go back button

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,23 +1,7 @@
-import { useSearchParams } from "react-router-dom";
-
 const SearchForm = ({ onSubmit }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const query = searchParams.get("query") ?? "";
-
-  const queryStringChanger = query => {
-    const nextParam = query !== "" ? { query } : {};
-    setSearchParams(nextParam);
-  };
-
   return (
     <form onSubmit={onSubmit}>
-      <input
-        name="title"
-        type="text"
-        value={query}
-        onChange={e => queryStringChanger(e.currentTarget.value)}
-      />
+      <input name="title" type="text" />
       <button type="submit">Search</button>
     </form>
   );
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import MovieList from "../components/MovieList/MovieList";
 import { searchMoviesReq } from "../apiMovies";
 import Notifications from "../components/Notifications/Notifications";
@@ -7,21 +8,35 @@ import SearchForm from "../components/SearchForm/SearchForm";
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchQuery = searchParams.get("query") ?? "";
+
+  useEffect(() => {
+    if (searchQuery === "") return;
+
+    const fetchMovies = async () => {
+      try {
+        const movies = await searchMoviesReq(searchQuery);
+        setMovies(movies);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchMovies();
+  }, [searchQuery]);
 
   const handleSubmit = async e => {
     e.preventDefault();
     const form = e.currentTarget;
-    try {
-      const movies = await searchMoviesReq(form.elements.title.value);
-      setMovies(movies);
-    } catch (error) {
-      setError(error.message);
-    }
+    const query = form.elements.title.value;
+    const nextParam = query !== "" ? { query } : {};
+    setSearchParams(nextParam);
   };
 
   return (
     <div>
-      <SearchForm onSubmit={handleSubmit} />
+      <SearchForm onSubmit={handleSubmit} searchQuery={searchQuery} />
       <h2>Films you search</h2>
       {error ? (
         <Notifications type="error" msg={error} />
